b7d8fc6231889e4f78696ab08fee7c6f01f17661
Leon Nguyen <leondnguyen@gmail.com>
2025-12-22 14:49:37 -0500

    functionality to instagram follow

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ function connect() {
                 document.getElementById("listen").innerHTML 
                 = "Listen to music and podcasts.";
             }
+
+function follow() {
+                document.getElementById("listen").innerHTML 
+                = "Follow us at @pianoroom for more content!";
+            }
