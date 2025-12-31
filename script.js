2c9d917d1e83ffb32243c25778045b80e5771d72
Leon Nguyen <leondnguyen@gmail.com>
2025-12-30 20:18:50 -0500

    added toggle buttons, formatting classes, css stylesheet

--- script.js
+++ script.js
@@ -7,3 +7,13 @@ function follow() {
                 document.getElementById("listen").innerHTML 
                 = "Follow us at @pianoroom for more content!";
             }
+
+function toggleBold() {
+                const el = document.getElementById("toggleText");
+                el.classList.toggle("bold");
+}
+
+function toggleItalic() {
+                const el = document.getElementById("toggleText");
+                el.classList.toggle("italic");
+}
